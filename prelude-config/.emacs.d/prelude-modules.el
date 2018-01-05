f5a3d0de7ca2ad8e7ecdfc793272cac3ca5f3bdc
Johannes Choo <jhanschoo@gmail.com>
2018-01-05 23:31:08 +0800

    edit emacs config

--- prelude-config/.emacs.d/prelude-modules.el
+++ prelude-config/.emacs.d/prelude-modules.el
@@ -3,7 +3,7 @@
 ;; Emacs IRC client
 (require 'prelude-erc)
 (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
-;; (require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm) ;; Interface for narrowing and search
 ;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
@@ -24,12 +24,12 @@
 (require 'prelude-js)
 (require 'prelude-latex)
 (require 'prelude-lisp)
-;; (require 'prelude-ocaml)
+(require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
-;; (require 'prelude-python)
-;; (require 'prelude-ruby)
-;; (require 'prelude-rust)
+(require 'prelude-python)
+(require 'prelude-ruby)
+(require 'prelude-rust)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
